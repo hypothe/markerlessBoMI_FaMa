092fedcdc317b9d3f811fb172686428ea0662f4f
hypothe <markeagle98@gmail.com>
2022-01-02 09:05:28 +0000

    <Add> autoremove docker container after closing

--- docker_run_scripts/run_bomi_wsl.sh
+++ docker_run_scripts/run_bomi_wsl.sh
@@ -4,7 +4,7 @@
 
 DISPLAY=$(grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0
 
-docker run -dit --gpus all --privileged --env NVIDIA_DRIVER_CAPABILITIES='graphics,compute,utility' \
+docker run -dit --rm --gpus all --privileged --env NVIDIA_DRIVER_CAPABILITIES='graphics,compute,utility' \
  --env DISPLAY=$DISPLAY \
  --env "PULSE_SERVER=${PULSE_SERVER}" -v /mnt/wslg/:/mnt/wslg/ \
  hypothe/bomi_fama:latest
